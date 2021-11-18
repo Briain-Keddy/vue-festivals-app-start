8b6da2c50faae91b51185ee9e208d3a8a0795161
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-18 13:59:26 +0000

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <b-col>
     <h2>Welcome to the Cloud Festivals Website</h2>
-    <div v-if="!loggedIn">
+    <div v-if="!$store.state.loggedIn">
       <input type="email" v-model="form.email" /> <br>
       <input type="password" v-model="form.password" /><br>
-      <button @click="login()">Submit</button>
+      <button @click="login(form)">Submit</button>
     </div>
     <p v-else>
       You are logged in
@@ -13,15 +13,11 @@
 </template>
 
 <script>
-import axios from 'axios';
-// import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions } from 'vuex'
+
 export default {
   name: "Home",
   components: {
-    // HelloWorld
-  },
-  props:{
-    loggedIn: Boolean
   },
   data(){
     return{
@@ -32,21 +28,7 @@ export default {
     }
   },
   methods:{
-    login(){
-      axios
-           .post('https://festivals-api.herokuapp.com/api/users/login',{
-             email: this.form.email,
-             password: this.form.password
-           })
-           .then(response =>{
-             console.log(response.data.token)
-             this.$emit('login')
-           })
-           .catch(error => {
-             console.log(error)
-             console.log(error.response)
-           })
-    }
+    ...mapActions(['login'])
   }
 };
 </script>
